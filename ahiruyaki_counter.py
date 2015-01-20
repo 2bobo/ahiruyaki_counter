db21c0b0b7699eaa654612e639f618586aa96244
2bo <rheinbund@gmail.com>
2015-01-20 14:20:36 +0900

    メッセージ修正

--- ahiruyaki_counter.py
+++ ahiruyaki_counter.py
@@ -9,12 +9,10 @@ import datetime
 import time
 import ConfigParser
 import socket
-#import simplejson
 import struct
 import string
 import tweepy
 
-# ZABBIX Sender
 class ZabbixSender:
     zbx_header = 'ZBXD'
     zbx_version = 1
@@ -30,20 +28,16 @@ class ZabbixSender:
         if clock != None:
             add_data[u'clock'] = clock
         self.zbx_sender_data['data'].append(add_data)
-
-#        print self.zbx_sender_data['data']
         return self.zbx_sender_data
 
     def ClearData(self):
         self.zbx_sender_data['data'] = []
-
         return self.zbx_sender_data
 
     def __MakeSendData(self):
         zbx_sender_json = json.dumps(self.zbx_sender_data, separators=(',', ':'), ensure_ascii=False).encode('utf-8')
         json_byte = len(zbx_sender_json)
         self.send_data = struct.pack("<4sBq" + str(json_byte) + "s", self.zbx_header, self.zbx_version, json_byte, zbx_sender_json)
-#        print self.send_data
 
     def Send(self):
         self.__MakeSendData()
@@ -58,8 +52,6 @@ class ZabbixSender:
         so.close()
         tmp_data = struct.unpack("<4sBq" + str(len(recv_data) - struct.calcsize("<4sBq")) + "s", recv_data)
         recv_json = json.loads(tmp_data[3])
-#        print recv_json
-
         return recv_data
 
 
@@ -87,7 +79,6 @@ class ZabbixAPI(object):
     # auth key 発行用関数
     # 戻り値:ZABBIX API auth key
     def auth(self):
-        # json 認証用生成
         auth_post = json.dumps({
             'jsonrpc': '2.0',
             'method': 'user.login',
@@ -96,21 +87,14 @@ class ZabbixAPI(object):
                 'password': self.zbx_passwd},
             'auth':None,
             'id': 1})
-        # urllib2 へのリクエスト生成
         req = urllib2.Request(self.zbx_url, auth_post, self.headers)
-        # リクエストを送信
         f = urllib2.urlopen(req)
-        # コンテンツを受け取る
         str_value = f.read()
-        # セッションを閉じる
         f.close()
-        # コンテンツをJSONに整形
         value = json.loads(str_value)
-        # 認証成功ならば authkey を返す
         try:
             self.zbx_auth = value["result"]
             return value["result"]
-        # だめだったら認証失敗を返す
         except:
             print "Authentication failure"
             return 0
@@ -118,19 +102,10 @@ class ZabbixAPI(object):
         storage.close()
 
     def send(self, json_data):
-        # 認証キーの追記
-        #json_data["auth"] = self.zbx_auth
-        # json 化
-        #req_json = json
-        #  urllib2 へのリクエスト生成
         req = urllib2.Request(self.zbx_url, json_data, self.headers)
-        # リクエストを送信
         f = urllib2.urlopen(req)
-        # コンテンツを受け取る
         str_value = f.read()
-        # セッションを閉じる
         f.close()
-        # コンテンツをJSONに整形
         dict_value = json.loads(str_value)
         return dict_value
 
@@ -260,7 +235,7 @@ if __name__ == '__main__':
 #    print end_time
     yakishi_list = {}
 
-    postdata = unicode(twdate.strftime("%Y年%m月%d日%H時台のあひる焼きカウンター\n(テスト運用中)\n"),'utf-8', 'ignore')
+    postdata = unicode(twdate.strftime("%Y年%m月%d日%H時台に焼かれたあひるの数\n(テスト運用中)\n"),'utf-8', 'ignore')
     auth = authorize(conf)
     api = tweepy.API(auth_handler=auth)
     keywords = [u"あひる焼き", u"-RT"]
@@ -268,7 +243,7 @@ if __name__ == '__main__':
     for tweet in api.search(q=query, count=1000):
 #        print type(tweet.created_at)
         textdata = tweet.text.encode('utf-8')
-        if textdata.find("あひる焼き") != -1: #and textdata.find("あひる焼きカウンター") == -1:
+        if textdata.find("あひる焼き") != -1 and textdata.find("あひる焼きカウンター") == -1:
             if start_time < tweet.created_at < end_time :
                 if not tweet.user.screen_name in yakishi_list:
                     itemdata = create_zbx_item(tweet.user.screen_name, zbx_api, zbx_auth_key)
@@ -276,11 +251,13 @@ if __name__ == '__main__':
                 else:
                     yakishi_list[tweet.user.screen_name] += 1
 #                print tweet.created_at, tweet.user.screen_name, tweet.text
-
     for id, count in yakishi_list.items():
-        item_key = "ahiruyaki.hcount." + id
-        put_zbx_sender(conf.get("zabbix","ip"), item_key, "ahiruyaki", 1)
-        postdata = postdata +  u"@" + id + ": " + str(count) + u"焼き\n"
+        if len(id) == 0:
+            postdata = postdata +  u"あひるは焼かれなかった\n"
+        else:
+            item_key = "ahiruyaki.hcount." + id
+            put_zbx_sender(conf.get("zabbix","ip"), item_key, "ahiruyaki", 1)
+            postdata = postdata +  u"@" + id + ": " + str(count) + u"焼き\n"
 
     #post twitter
 #    print postdata
